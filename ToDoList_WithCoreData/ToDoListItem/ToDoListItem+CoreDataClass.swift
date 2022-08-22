ea0c1b25e73150590503827b71d051310d2f11ce
egemenkarakayaa <egemenkarakayaa@gmail.com>
2022-08-22 20:23:12 +0300

    Added functions related to core data.

---
+++ ToDoList_WithCoreData/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoList_WithCoreData
+//
+//  Created by Egemen Karakaya on 22.08.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ ToDoList_WithCoreData/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  ToDoList_WithCoreData
+//
+//  Created by Egemen Karakaya on 22.08.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
