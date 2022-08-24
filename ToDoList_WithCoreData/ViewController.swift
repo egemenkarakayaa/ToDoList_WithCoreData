05e549c3bfe70bd32ccf75e1b2f20ea98b21060f
egemenkarakayaa <egemenkarakayaa@gmail.com>
2022-08-24 12:28:11 +0300

    Added functions to edit or delete to-do list items.

--- ToDoList_WithCoreData/ViewController.swift
+++ ToDoList_WithCoreData/ViewController.swift
@@ -12,14 +12,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    
     let tableView: UITableView = {
          let table = UITableView()
          table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
          return table
     }()
-
     
+
     private var models = [ToDoListItem]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +29,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         title = "To Do List"
         
         view.addSubview(tableView)
+        getAllItems()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
@@ -35,7 +38,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    
     @objc private func didTapAdd() {
+        
         let alert = UIAlertController(title: "New Mission", message: "Enter New Mission", preferredStyle: .alert)
         
         alert.addTextField(configurationHandler: nil)
@@ -52,6 +57,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     
+    // Table View Functions
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
@@ -62,6 +69,45 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = model.name
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let item = models[indexPath.row]
+        
+        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
+        
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+            
+            let alert = UIAlertController(title: "Edit Item", message: "Edit Your Mission", preferredStyle: .alert)
+            
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
+                    return
+                }
+                
+                self?.updateItem(item: item, newName: newName)
+                
+            }))
+            
+            self.present(alert, animated: true)
+            
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+
+        
+        present(sheet, animated: true)
+        
+    }
+    
 
     // Core Data Functions
     
@@ -80,6 +126,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func createItem(name: String){
+        
         let newItem = ToDoListItem(context: context)
         newItem.name = name
         newItem.createdAt = Date()
@@ -116,7 +163,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         
     }
-    
     
     
     
